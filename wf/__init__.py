3635bd4a7f7e8726ba0fb6d70aceeb27e3ebc30e
Shivaramakrishna Srinivasan <shivaramakrishna.srinivasan@gmail.com>
2022-06-12 17:01:36 +0530

    Docstring specifications

--- wf/__init__.py
+++ wf/__init__.py
@@ -18,6 +18,10 @@ class Material(Enum):
     rna = "RNA"
     rna95 = "RNA95"
 
+class Ensemble(Enum):
+    stacking = "stacking"
+    nostacking = "nostacking"
+
 
 # Define Model() object and parameters   
 
@@ -26,14 +30,22 @@ def utilities(
     strand1: str = "ATGC",
     strand2: str = "ATGC",
     structure: str = "....",
-    material: Material = Material.dna,
+    pfunc: bool = False,
+    structure_energy: bool = False,
+    structure_probability: bool = False,
+    pairs: bool = False,
+    mfe: bool = False,
+    ensemble_size: bool = False,
+    defect: bool = False,
+    material: str = Material.dna,
+    ensemble: str = Ensemble.stacking,
     temperature: float = 37.0,
     sodium: float = 1.0,
     magnesium: float = 0.0,
     outputDir: str = "output"
 ) -> LatchDir:
 
-    nt_model = Model(material=str(material), celsius=temperature, sodium=sodium, magnesium=magnesium)
+    nt_model = Model(material=material, celsius=temperature, sodium=sodium, magnesium=magnesium)
     # partition_function = pfunc(strands=['CCC', 'GGG'], model=nt_model)
 
     content = f"""
@@ -48,11 +60,19 @@ def utilitiesNUPACK(
     strand1: str = "ATGC",
     strand2: str = "ATGC",
     structure: str = "....",
-    material: Material = Material.dna,
+    pfunc: bool = False,
+    structure_energy: bool = False,
+    structure_probability: bool = False,
+    pairs: bool = False,
+    mfe: bool = False,
+    ensemble_size: bool = False,
+    defect: bool = False,
+    material: str = Material.dna,
+    ensemble: str = Ensemble.stacking,
     temperature: float = 37.0,
     sodium: float = 1.0,
     magnesium: float = 0.0,
-    outputDir: str = "output"
+    outputDir: str = "nupack-utils-run"
 ) -> LatchDir:
     """Carry out NUPACK Utilities functions on a complex of sequences
 
@@ -104,29 +124,19 @@ def utilitiesNUPACK(
             __metadata__:
                 display_name: "Nucleic Acid Type"
                 _tmp:
-                    section_title: Model Specification
+                    section_title: Sequence and Structure Details
                 appearance:
                     comment: "Choose between DNA and RNA free energy parameter sets. Default is 'rna', based on Matthews et al., 1999"
-
-        temperature:
-            __metadata__:
-                display_name: "Temperature (in degree Celsius)"
-                appearance:
-                    comment: "Temperature of system. Default is 37 °C"
 
         strand1:
             __metadata__:
                 display_name: "Strand 1 (as nucleotides)"
-                _tmp:
-                    section_title: Strand Details
                 appearance:
                     comment: "Enter the nucleotide sequence of the first strand"
 
         strand2:
             __metadata__:
                 display_name: "Strand 2 (as nucleotides)"
-                _tmp:
-                    section_title: Strand Details
                 appearance:
                     comment: "Enter the nucleotide sequence of the second strand"
 
@@ -135,6 +145,67 @@ def utilitiesNUPACK(
                 display_name: "Structure of Complex (in regular/extended dot-bracket notation)"
                 appearance:
                     comment: "Enter the dot bracket notation of the intended structure"
+
+        pfunc:
+            __metadata__:
+                display_name: "Partition Function"
+                _tmp:
+                    section_title: Choose NUPACK Utility Functions
+                appearance:
+                    comment: "Compute the partition function https://www.google.com"
+
+        structure_energy:
+            __metadata__:
+                display_name: "Structure Free Energy"
+                appearance:
+                    comment: "Calculate the structure energy"
+
+        structure_probability:
+            __metadata__:
+                display_name: "Equilibrium Structure Probability"
+                appearance:
+                    comment: ""
+
+        pairs:
+            __metadata__:
+                display_name: "Equilibrium Base Pairing Probabilities"
+                appearance:
+                    comment: ""
+
+        mfe:
+            __metadata__:
+                display_name: "MFE Proxy Structure(s)"
+                appearance:
+                    comment: ""
+
+        ensemble_size:
+            __metadata__:
+                display_name: "Complex Ensemble Size"
+                appearance:
+                    comment: ""
+
+        defect:
+            __metadata__:
+                display_name: "Complex Ensemble Size"
+                appearance:
+                    comment: ""
+
+        ensemble:
+            __metadata__:
+                display_name: "Ensemble Type"
+                _tmp:
+                    section_title: Additional Model Specification
+                    hidden: true
+                appearance:
+                    comment: "Choose between stacking and non stacking ensemble states. Default is set to 'stacking'."
+
+        temperature:
+            __metadata__:
+                display_name: "Temperature (in degree Celsius)"
+                _tmp:
+                    hidden: true
+                appearance:
+                    comment: "Temperature of system. Default is 37 °C"
 
         sodium:
             __metadata__:
@@ -164,9 +235,17 @@ def utilitiesNUPACK(
     strand1=strand1,
     strand2=strand2,
     structure=structure,
+    pfunc=pfunc,
+    structure_energy=structure_energy,
+    structure_probability=structure_probability,
+    pairs=pairs,
+    mfe=mfe,
+    ensemble_size=ensemble_size,
+    defect=defect,
     material=material,
+    ensemble=ensemble,
     temperature=temperature,
     sodium=sodium,
     magnesium=magnesium,
-    outputDir=outputDir
+    outputDir=outputDir,
     )
